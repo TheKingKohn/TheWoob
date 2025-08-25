84e6afc59f9cd458f7d3e18b54d3080726e6f1c2
G Kohn <gavinkohn@aol.com>
2025-08-24 23:47:10 -0400

    Fix Next.js Image import and confirm hook dependencies are correct

--- src/pages/admin/DealAdminCard.tsx
+++ src/pages/admin/DealAdminCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { toast } from "../../components/Toast";
+import Image from "next/image";
 
 export default function DealAdminCard({ deal, onUpdate, onDelete }: any) {
 	const [editing, setEditing] = useState(false);
